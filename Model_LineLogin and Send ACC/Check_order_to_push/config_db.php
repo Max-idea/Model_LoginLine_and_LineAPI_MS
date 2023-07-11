e90b23006d929576da82d9c2822536bf8cda8179
Max-idea <136289725+Max-idea@users.noreply.github.com>
2023-07-11 17:07:35 +0700

    Update config_db.php

--- Model_LineLogin and Send ACC/Check_order_to_push/config_db.php
+++ Model_LineLogin and Send ACC/Check_order_to_push/config_db.php
@@ -1,6 +1,6 @@
 <?php
 // Create a new MySQL connection
-$conn = new mysqli('127.0.0.1', 'up0pykdpcofgq', 'bggssjm1cg4b', 'dbnqpjg8oxxuyj');
+$conn = new mysqli('', 'up0pykdpcofgq', 'bggssjm1cg4b', 'dbnqpjg8oxxuyj');
 
 // Check the connection
 if ($conn->connect_error) {
